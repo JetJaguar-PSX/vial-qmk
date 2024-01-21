fe914067c7f733621b64772058bb19094cc52829
Jetjaguar-PSX <s195056@matsu.shimane-u.ac.jp>
2024-01-21 14:31:11 +0900

    upload uf2 file

---
+++ keyboards/ergogridgo_bmp/ergogridgo.c
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "ergogridgo.h"
+
+#include "bmp.h"
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    bool cont = process_record_bmp(keycode, record);
+
+    if (cont) {
+        process_record_user(keycode, record);
+    }
+
+    return cont;
+}
+
+void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
+    uint8_t *command_id = &(data[0]);
+    if (*command_id == id_set_keyboard_value) {
+        // This version does not handle save flag
+    } else {
+        *command_id = id_unhandled;
+    }
+}
