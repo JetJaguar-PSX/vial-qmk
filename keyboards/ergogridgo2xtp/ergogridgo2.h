b095ff0345e23762cb8147da347c6b972188f8dc
Shiitake <64678157+JetJaguar-PSX@users.noreply.github.com>
2024-05-01 18:19:37 +0900

    Delete keyboards/ergogridgo2xtp directory

--- keyboards/ergogridgo2xtp/ergogridgo2.h
+++
@@ -1,6 +0,0 @@
- #pragma once
-
-#include "quantum.h"
-
-extern void (*pointing_device_set_button)(uint8_t btn);
-extern void (*pointing_device_clear_button)(uint8_t btn);
--- keyboards/ergogridgo2xtp/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0xB2, 0x53, 0x60, 0xCB, 0x4A, 0x83, 0xB8, 0xD1}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
